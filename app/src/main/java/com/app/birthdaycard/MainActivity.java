955d2814f4bfaadaadd326456d66fcc9601ddd8f
Miayangmy <miayangmy@163.com>
2020-03-21 11:53:34 +0800

    温度转换程序

--- app/src/main/java/com/app/birthdaycard/MainActivity.java
+++ app/src/main/java/com/app/birthdaycard/MainActivity.java
@@ -3,12 +3,45 @@ package com.app.birthdaycard;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import static com.app.birthdaycard.R.id.button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText input = (EditText) findViewById(R.id.editText1);
+        String str = input.getText().toString();
+
+        TextView output = (TextView) findViewById(R.id.textView3);
+
+        Button btn = (Button) findViewById(button);
+        btn.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        Log.i("click","onClick");
+
+        EditText input = (EditText) findViewById(R.id.editText1);
+        String str = input.getText().toString();
+        double s = Double.valueOf(str);
+
+        TextView output = (TextView) findViewById(R.id.textView3);
+
+        double r = 0;
+        r = 1.8*s + 32;
+        String result = Double.toString(r);
+
+        output.setText(result);
+    }
+
 }
